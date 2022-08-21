cb96ad3237c0eda9c2e19922e4ca84a303a6a9f3
yashchinchole <iamyash337@gmail.com>
2022-08-21 11:15:03 +0530

    main

---
+++ 1_Is_it_hot_or_cold.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int c;
+        cin >> c;
+        (c > 20) ? cout << "HOT\n" : cout << "COLD\n";
+    }
+    return 0;
+}
