237e936139d66b9c8fb635ca7e561dbb64ad6ec1
yashchinchole <iamyash337@gmail.com>
2022-10-17 19:37:03 +0530

    main

---
+++ 39_AC_Temperature.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (b < a || b < c)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+
+    return 0;
+}
